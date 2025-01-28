68fc1046b24901ad2cefe9ae20c9ebcff22287fe
Nicolas Vaillancourt <nvaillan@ucsc.edu>
2025-01-27 18:35:26 -0800

    Added name and changes to file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+// Name: Nicolas Vaillancourt
+// Date: 1-27-25
+//
+// Changes:
+//
+// Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+// Implement mouse control for player movement and left mouse click to fire (5)
+// Display the time remaining (in seconds) on the screen (3)
+// Implement parallax scrolling for the background (3)
+// Create a new scrolling tile sprite for the background (1)
+
+
 "use strict"
 
 let config = {
@@ -9,7 +22,7 @@ let config = {
         // mode: Phaser.Scale.FIT, // Ensures the game scales proportionally to fit
         autoCenter: Phaser.Scale.CENTER_BOTH // Centers the game both horizontally and vertically
     },
-    pixelArt: true
+    pixelArt: true,
 }
 
 let game = new Phaser.Game(config)
